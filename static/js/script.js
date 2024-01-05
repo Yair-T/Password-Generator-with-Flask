671e7a0326120540c49b1a9d5d31f6d20a468aaf
YairT <108692316+Yair-T@users.noreply.github.com>
2024-01-05 14:23:59 +0200

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,1 @@
-// The code by Yair-T | https://youtube.com/@Yair-T | https://github.com/Yair-T
-let password = document.querySelector(".password-input"); 
-let btn = document.querySelector(".material-symbols-outlined"); 
-gen_btn = document.querySelector(".btn"); 
-btn.addEventListener("click", function () { 
-    password.select(); password.setSelectionRange(0, 99999); 
-    navigator.clipboard.writeText(password.value);
-     btn.innerText = "done"; setTimeout(() => { 
-        btn.innerText = 'content_copy'; 
-    }, 1500); 
-});
+/* The code by Yair-T | https://youtube.com/@Yair-T | https://github.com/Yair-T*/let password=document.querySelector(".password-input"),btn=document.querySelector(".material-symbols-outlined");gen_btn=document.querySelector(".btn"),btn.addEventListener("click",(function(){password.select(),password.setSelectionRange(0,99999),navigator.clipboard.writeText(password.value),btn.innerText="done",setTimeout((()=>{btn.innerText="content_copy"}),1500)}));
